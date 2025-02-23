a1fff5f2b27a919f2c8f5b5218dba095877bcac7
nishitkirani2020 <nishitkirani2020@gmail.com>
2025-02-23 11:52:58 +0000

    Update icons for detailed survey page

--- src/pages/detailed-survey/index.tsx
+++ src/pages/detailed-survey/index.tsx
@@ -4,15 +4,15 @@ import Header from "@/components/Header";
 import Link from "next/link";
 
 const categories = [
-  { name: "Governance and Policies", link: "/detailed-survey/governance", img: "/icons/governance.svg" },
-  { name: "Technical", link: "/detailed-survey/technical", img: "/icons/technical.svg" },
-  { name: "Data Security", link: "/detailed-survey/data-security", img: "/icons/data-security.svg" },
-  { name: "Access Control", link: "/detailed-survey/access-control", img: "/icons/access-control.svg" },
+  { name: "Governance and Policies", link: "/detailed-survey/governance", img: "/images/governance.svg" },
+  { name: "Technical", link: "/detailed-survey/technical", img: "/images/technical.svg" },
+  { name: "Data Security", link: "/detailed-survey/data-security", img: "/images/data-security.svg" },
+  { name: "Access Control", link: "/detailed-survey/access-control", img: "/images/access-control.svg" },
 ];
 
 const secondRowCategories = [
-  { name: "Monitoring and Detection", link: "/detailed-survey/monitoring", img: "/icons/monitoring.svg" },
-  { name: "Training and Awareness", link: "/detailed-survey/training", img: "/icons/training.svg" },
+  { name: "Monitoring and Detection", link: "/detailed-survey/monitoring", img: "/images/monitoring.svg" },
+  { name: "Training and Awareness", link: "/detailed-survey/training", img: "/images/training.svg" },
 ];
 
 const DetailedSurvey = () => {
